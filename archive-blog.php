6dabc15b456accf29f9e21eee6e7b8da5c326c04
Latoya <76912011+latoyaln@users.noreply.github.com>
2025-01-21 13:54:38 +0100

    #3 BEM naming

--- archive-blog.php
+++ archive-blog.php
@@ -3,15 +3,17 @@
 get_header();
 
 ?>
-<div class="container">
+<div class="blog-container">
     <?php
         if ( have_posts() ) :
+            echo '<div class="blog-grid">';
             while ( have_posts() ) :
                 the_post();
                 include locate_template('partials/post.php');
             endwhile;
+            echo '</div>';
         else :
-            echo '<p>No posts found.</p>';
+            echo '<p class="blog-container__message">No posts found.</p>';
         endif;
     ?>
 </div>
